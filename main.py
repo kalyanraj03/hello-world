e005d5d671764f4d9c621e9fbb42090e3c31c581
kalyanraj <kalyanraj3003@gmail.com>
2023-11-24 12:22:05 +0530

    added comments to the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def stocks(arr):
     min_price=arr[0]
     max_price=0
 
+
     for i in range(1,len(arr)):
         if arr[i]<min_price:
             min_price=arr[i]
@@ -9,7 +10,7 @@ def stocks(arr):
             if max_price< arr[i]-min_price:
                 max_price=arr[i]-min_price
 
-    return max_price, min_price
+    return max_price, min_price # return result called max_price
 
 arr=[4,3,8,1,11]
 
